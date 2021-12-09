3e39084d3f594e432a1ca8408900c29c8b609217
bertinc <bertinc@gmail.com>
2021-12-09 10:00:58 -0700

    Exclusions are optional, but I want to keep a history of the ones we use. This is the final SQL script at the time of generating the assignments for 2021

--- hat_trick/init_db.sql
+++ hat_trick/init_db.sql
@@ -13,12 +13,12 @@ CREATE TABLE people (
 
 -- the exclude column is a pipe delimited list of first names that a person should not get
 -- example: 'Natty|Gabby'
-INSERT INTO people VALUES (NULL, 'Robert', 'Rallison', 1, 'm', 'ral', 0, 'Lori');
-INSERT INTO people VALUES (NULL, 'Tori', 'Rallison', 1, 'f', 'ral', 0, 'Chris|Lexy');
+INSERT INTO people VALUES (NULL, 'Robert', 'Rallison', 1, 'm', 'ral', 0, 'Lori|Chris|Gabby|Colton');
+INSERT INTO people VALUES (NULL, 'Tori', 'Rallison', 1, 'f', 'ral', 0, 'Chris|Lexy|Gabby|Colton');
 INSERT INTO people VALUES (NULL, 'Jack', 'Rallison', 0, 'm', 'ral', 0, '');
 INSERT INTO people VALUES (NULL, 'Brianna', 'Rallison', 0, 'f', 'ral', 0, '');
-INSERT INTO people VALUES (NULL, 'Gabby', 'Averatt', 1, 'f', 'ave', 0, '');
-INSERT INTO people VALUES (NULL, 'Colton', 'Averatt', 1, 'm', 'ave', 0, '');
+INSERT INTO people VALUES (NULL, 'Gabby', 'Averatt', 1, 'f', 'ave', 0, 'Robert|Tori');
+INSERT INTO people VALUES (NULL, 'Colton', 'Averatt', 1, 'm', 'ave', 0, 'Robert|Tori');
 INSERT INTO people VALUES (NULL, 'Elanore', 'Averatt', 0, 'f', 'ave', 1, '');
 INSERT INTO people VALUES (NULL, 'Lori', 'Martin', 1, 'f', 'mar', 0, '');
 INSERT INTO people VALUES (NULL, 'Lexy', 'Peterson', 1, 'f', 'pet', 0, '');
