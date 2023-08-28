586e39d64efe188877724af0ba97b2bc270fdf94
Robert Rallison <bertinc@gmail.com>
2023-08-28 15:50:39 -0600

    a little restructuring and more commments

--- web_scraping_for_fun/card_count.py
+++ web_scraping_for_fun/card_count.py
@@ -2,16 +2,17 @@
 Author: Robert Rallison
 Created: 2023-08-28
 
-Trying to find the number of cards in the huntress pledge which includes the red box. For this I am using some html response
-scraping with BeautifulSoup. The path to the cards sections is not super clear at first when inspecting the pages. I decided
-to find all rows of a specific class and then filter out anything that doesn't have the word 'Cards' in the text. This almost
-worked perfectly. I had one row that was actually just a part of the main components list that mentions cards. So that is the
-only static skip. This script, as is, works out of the box on two of their component list pages.
+WHY: Trying to find the number of cards in the huntress pledge which includes the red box (and black box but I already sleaved it). I did
+some searching but as soon as I came accross the component list, I stopped looking and just decided to scrape it for what I needed. Honestly,
+I was having no luck finding any posts even on BGG to get this number and I wanted to make sure I purchased enough sleaves.
 
-TIPS: To add more pages, use the get_card_lists function by itself to print the row names. That was helpful for me to figure
+HOW: For this I am using some html response scraping with BeautifulSoup. The path to the cards sections is not super clear at first when
+inspecting the pages. I decided to find all rows of a specific class and then filter out anything that doesn't have the word 'Cards'
+in the text. This almost worked perfectly. I had one row that was actually just a part of the main components list that mentions
+cards. So that is the only static skip. This script, as is, works out of the box on three of their component list pages.
+
+TIPS: To test another page, use the get_card_lists function by itself to print the row names. That was helpful for me to figure
 out what I needed to do differently in my logic between the pages.
-
-NOTE: It also included the black box, but I already sleaved that one since it came early.
 """
 import requests
 from bs4 import BeautifulSoup
@@ -37,20 +38,26 @@ def main():
     huntress_cards = get_cards(rows_huntress)
     redbox_cards = get_cards(rows_redbox)
     blackbox_cards = get_cards(rows_blackbox)
-    all_cards = huntress_cards + redbox_cards
+    all_cards = huntress_cards + redbox_cards  # intentionally not including Black Box here
 
-    # Just for fun, I guess
-    # longest_name = "Sothemir - The King's Champion (Alternate Form)"  # <-- found this with some throw away code I later deleted
-    # print_card_names(all_cards, len(longest_name))
+    # Just for fun: uncomment the below line if you want to print the card names
+    print_card_names(all_cards, "Huntress and Red Box Card Names")
 
     print(f"Number of cards in huntress box: {len(huntress_cards)}")
     print(f"Number of cards in red box: {len(redbox_cards)}")
     print(f"Total cards to sleave: {len(all_cards)}")
+
+    # This one I counted manually and it was correct so I think it's safe to assume the others are too.
     print(f"BONUS Number of cards in black box: {len(blackbox_cards)}")
 
 def get_rows(url):
     """
-    Since this was repeating code, I figured a may as well make it a function.
+    Since this was repeating code, I figured I may as well make it a function.
+
+    Args:
+        url (str): webpage to scrape
+    Returns:
+        Soup search results : all of the tags found when making the initial Soup request
     """
     response = requests.get(url, timeout=30)
     rows = BeautifulSoup(response.content, "lxml").find_all(True, {"class": "elementor-row"})
@@ -59,6 +66,11 @@ def get_rows(url):
 def get_cards(rows):
     """
     Make sense of the list dictionary we get from get_card_lists and build a list of card names to return.
+
+    Args:
+        rows (tags): all of the tags found when making the initial Soup request
+    Returns:
+        List: all of the card names with white space stripped away
     """
     card_lists = get_card_lists(rows)
 
@@ -74,6 +86,11 @@ def get_card_lists(rows):
     """
     Loop through, filtering out rows that do not contain cards and then scrape
     out the list items to itterate for a count.
+
+    Args:
+        rows (tags): all of the tags found when making the initial Soup request
+    Returns:
+        Dictionary: filtered down rows and the found list items in them
     """
     all_list_items = {}
     count = 1
@@ -87,12 +104,32 @@ def get_card_lists(rows):
         count += 1
     return all_list_items
 
-def print_card_names(cards, justify=50):
+def get_longest_name(cards):
+    """
+    Nice a quick way to grab the longest card in the list for printing a formatted table.
+
+    Args:
+        cards (list): list of card names
+    Returns:
+        int: the length of the longest card in the list
+    """
+    longest = 0
+    for card in cards:
+        if len(card) > longest:
+            longest = len(card)
+    return longest
+
+def print_card_names(cards, title="Pretty printing the cards."):
     """
     For a sort of pretty print of all the cards? This will make it look like there are duplicates, but that's just because
     I did not care to save the categories. Some cards are actually for crossovers to other games in the Catacombs universe.
+
+    Args:
+        cards (list): list of card names
+        title (str): a title at the top of the table
     """
     MAX_COLS = 3
+    justify = get_longest_name(cards)
 
     # This is just for looks and based on the length of the longest name
     break_line = "-" * (justify + MAX_COLS) * MAX_COLS
@@ -111,10 +148,11 @@ def print_card_names(cards, justify=50):
     
     # 2: Loop through those rows, join them into a nice printable string, and print them
     print(break_line)
-    print("A semi-nice and semi-readable list of all of the cards")
+    print(title.center(len(break_line)))
     for row in rows:
         print(break_line)
-        row_text = ''.join(f"| {card.ljust(justify)} " for card in row )
+        # build a nice row of names
+        row_text = ''.join(f"| {card.ljust(justify)} " for card in row)
         print(row_text)
     print(break_line)
 
